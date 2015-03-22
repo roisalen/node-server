e09281a9954c3376b28c2f551f9ac6e992cd1e4e
stianlagstad <stianlagstad@gmail.com>
2015-03-22 17:47:35 +0100

    removed comment - it waas annoying

--- models/speaker.js
+++ models/speaker.js
@@ -1,7 +1,7 @@
 var SpeakerQueue = require("./speakerqueue");
 
 module.exports = function(name, number, sex, group) {
-	console.log(name);
+	// console.log(name);
 	this.name = name;
 	this.number = number;
 	this._id = number;
