dc31181b6a77fb81326dfde112ab09d800db2380
Christian S <velfundert@gmail.com>
2016-03-26 15:12:35 +0100

    Revert "server bruker ssl"
    This reverts commits
    
    - a5d6e19197084ff136b44dea27c3066bafc7c69e.
    - 844fb4654eb0c2200c7b55d631e42f856499342c.

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 var express = require('express')
-var https = require('https')
-var fs = require('fs')
 var app = express()
 var bodyParser = require('body-parser')
 var cors = require('cors')
@@ -12,6 +10,13 @@ var MessageResource = require('./message-resource');
 var RepresentativesResource = require('./representatives-resource');
 var StatisticsResource = require('./statistics-resource');
 var OrganisationResource = require('./organisation-resource');
+
+//Set server
+var server = app.listen(8080, function() {
+	var host = server.address().address
+	var port = server.address().port
+	console.log('Listening at http://%s:%s', host, port)
+})
 
 //Use bodyparser and cors
 app.use(bodyParser.json())
@@ -49,12 +54,3 @@ app.delete('/organisations/IMSURE/:id', OrganisationResource.delete)
 app.get('/statistics/:field', StatisticsResource.getRankedListByField)
 app.get('/statistics/:field/:fromDate', StatisticsResource.getRankedListByField)
 app.get('/statistics/:field/:fromDate/:toDate', StatisticsResource.getRankedListByField)
-
-var options = {
-    key  : fs.readFileSync('privkey.pem'),
-    cert : fs.readFileSync('cert.pem')
-}
-
-https.createServer(options, app).listen(4443, '', null, function() {
-    console.log('Securely listening at https://%s:%s', this.address().address, this.address().port);
-});
